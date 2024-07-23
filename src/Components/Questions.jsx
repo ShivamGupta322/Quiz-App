1bc6197afbb6158f786c2835f6440ab546ba232b
ShivamGupta322 <guptashivamsg02@gmail.com>
2024-07-23 16:59:17 +0530

    added style to previous button

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -5,21 +5,21 @@ export const questions=[{question:'What is the primary purpose of an operating s
   option2 : 'B. To manage hardware and software resources',
    option3: 'C. To store data',
     option4: 'D. To connect to the internet',
-    ans:'2'},
+    ans: 2},
 
     {question:'Which data structure uses LIFO (Last In, First Out) order?',
         option1: 'A. Queue',
         option2 : 'B. Array',
          option3: 'C. Stack',
           option4: 'D. Linked List',
-          ans:'C'},
+          ans:3},
 
           {question:'In which layer of the OSI model does the IP protocol operate?',
             option1: 'A. To perform arithmetic operations',
             option2 : 'B. To manage hardware and software resources',
              option3: 'C. To store data',
               option4: 'D. To connect to the internet',
-              ans:'b'},
+              ans:2},
 
 
               {question:'Full Form of CPU?',
@@ -27,7 +27,7 @@ export const questions=[{question:'What is the primary purpose of an operating s
                 option2 : 'B. Central processing Unit',
                  option3: 'C. To store data',
                   option4: 'D. To connect to the internet',
-                  ans:'b'},
+                  ans:3},
 
 
                   {question:'Full form of css?',
@@ -35,6 +35,6 @@ export const questions=[{question:'What is the primary purpose of an operating s
                     option2 : 'B. xyz',
                      option3: 'C. To store data',
                       option4: 'D. To connect to the internet',
-                      ans:'a'},
+                      ans:1},
 ]
 
